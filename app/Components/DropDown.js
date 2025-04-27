2e1ece3cfa5695d2a8705890791c8b3b9f0f170b
vijay karena <45507500+vijaykarena@users.noreply.github.com>
2025-04-27 21:55:35 +0530

    Update DropDown.js
    api change

--- app/Components/DropDown.js
+++ app/Components/DropDown.js
@@ -53,7 +53,7 @@ function DropDown(props) {
     // console.log(selectedQuiz._id);
 
     const res = await fetch(
-      `http://localhost:3000/api/quizzes?id=${selectedQuiz._id}`,
+      `/api/quizzes?id=${selectedQuiz._id}`,
       {
         method: "DELETE",
         headers: {
